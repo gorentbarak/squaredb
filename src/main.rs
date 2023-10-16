f2642620df73b0371002411cf3ceb290c50780f6
Goren Barak <gorenbk@riseup.net>
2023-10-16 19:10:45 -0400

    fixed wrong url in message

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ pub mod table_management;
 #[tokio::main]
 async fn main() {
     const MESSAGE: [&str; 3] = ["SquareDB",
-    "To get a table from the database, go to https://localhost:3000/get_table/<table_name>",
-    "To send a table to the databse, go to https://localhost:3000/post_table and send JSON-encoded content with the table's data and a request header Content-Type: application/json"];
+    "To get a table from the database, go to http://localhost:3000/get_table/<table_name>",
+    "To send a table to the databse, go to http://localhost:3000/post_table and send JSON-encoded content with the table's data and a request header Content-Type: application/json"];
+
     for i in MESSAGE.iter() {
         println!("{}", i);
     }
+
     server::main().await;
 }
