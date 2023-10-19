ca7c8f1b2c989d2c4222aa1a9266b3cd8a2225bb
Goren Barak <gorenbk@riseup.net>
2023-10-19 09:16:33 -0400

    added array length as 4

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ pub mod storage;
 pub mod table_management;
 #[tokio::main]
 async fn main() {
-    const MESSAGE: [&str; 3] = ["SquareDB",
+    const MESSAGE: [&str; 4] = ["SquareDB",
     "To get a table from the database, go to http://localhost:3000/get_table/<table_name>.",
     "To send a table to the databse, go to http://localhost:3000/post_table and send JSON-encoded content with the table's data and a request header Content-Type: application/json.",
     "To drop a table in the database, go to http://localhost:3000/drop_table/<table_name>."];
